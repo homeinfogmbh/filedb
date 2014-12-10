bf531ecee570019ce73f70f8624fe702e7f86595
Richard Neumann <r.neumann@homeinfo.de>
2014-12-10 16:02:03 +0100

    fix

--- filedb/__init__.py
+++ filedb/__init__.py
@@ -1,8 +1,7 @@
 """
 HOMEINFO's file database
 """
-from .file import File
-from .error import ChecksumMismatch, FilesizeMismatch
+from .file import File, ChecksumMismatch
 
 __date__ = '02.12.2014'
 __author__ = 'Richard Neumann <r.neumannr@homeinfo.de>'
