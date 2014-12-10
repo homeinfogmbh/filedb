19885cc41d18a9bd5206f20735a13bbea4edbf53
Richard Neumann <r.neumann@homeinfo.de>
2014-12-10 16:07:30 +0100

    fix

--- filedb/__init__.py
+++ filedb/__init__.py
@@ -5,5 +5,5 @@ from .file import File, ChecksumMismatch
 
 __date__ = '02.12.2014'
 __author__ = 'Richard Neumann <r.neumannr@homeinfo.de>'
-__all__ = ['File', 'ChecksumMismatch', 'FilesizeMismatch']
+__all__ = ['File', 'ChecksumMismatch']
 __tables__ = [File]
