9cb1228515a706171f290975464dbe632550aa9d
Richard Neumann <mail@richard-neumann.de>
2014-12-12 17:53:35 +0100

    fix

--- setup.py
+++ setup.py
@@ -22,5 +22,10 @@ except OperationalError:
     print('WARNING: No database access - Won\'t create any tables')
 else:
     for table in __tables__:
-        print('Creating table', table)
-        table.create_table(fail_silently=True)
+        print('Creating table', table, end='\t')
+        try:
+            table.create_table(fail_silently=True)
+        except OperationalError:
+            print('[failed]')
+        else:
+            print('[done]')
