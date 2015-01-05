c43f6a3fb3338e372c843f04f27c07957db6d102
Richard Neumann <r.neumann@homeinfo.de>
2015-01-05 11:29:20 +0100

    fixes

--- filedb/file.py
+++ filedb/file.py
@@ -4,48 +4,52 @@ Abstract base classes for HOMEINFO's file database
 from .abc import FileDBModel
 from homeinfo.util import MIMEUtil
 from peewee import CharField, IntegerField
-from os.path import join, basename, dirname
+from os.path import basename, dirname
 from os import unlink
 from hashlib import sha256
+from base64 import b64encode
 
 __date__ = '02.12.2014'
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
-__all__ = ['ChecksumMismatch', 'File']
+__all__ = ['ChecksumMismatch', 'sha256sum', 'File']
 
 
 class ChecksumMismatch(Exception):
     """Indicates inconsistency between file checksums"""
-    def __init__(self, actual_value, target_value):
-        """Sets acual and target value"""
-        self._actual_value = actual_value
+    def __init__(self, target_value, actual_value):
+        """Sets target and actual value"""
         self._target_value = target_value
-
-    @property
-    def actual_value(self):
-        """Returns the actual value"""
-        return self._actual_value
+        self._actual_value = actual_value
 
     @property
     def target_value(self):
         """Returns the target value"""
         return self._target_value
 
+    @property
+    def actual_value(self):
+        """Returns the actual value"""
+        return self._actual_value
+
     def __str__(self):
         """Converts to a string"""
         return '\n'.join(['File checksums do not match',
-                          ' '.join(['    actual:', str(self.actual_value)]),
-                          ' '.join(['    target:', str(self.target_value)])])
+                          ' '.join(['    target:', str(self.target_value)]),
+                          ' '.join(['    actual:', str(self.actual_value)])])
+
+
+def sha256sum(data):
+    """Creates a checksum string of the respective data"""
+    return str(sha256(data).hexdigest())
 
 
 class File(FileDBModel):
     """
     A file entry
     """
-    dirname = CharField(255)
-    """The base directory of the file"""
-    basename = CharField(255)
-    """The file's base name"""
-    mimetype = CharField(255)
+    _name = CharField(255, db_column='name')
+    """The file's full path"""
+    _mimetype = CharField(255, db_column='mimetype')
     """The file's MIME type"""
     _sha256sum = CharField(69, db_column='sha256sum')
     """A SHA-256 checksum"""
@@ -55,36 +59,47 @@ class File(FileDBModel):
     """Amount of hardlinks on this file"""
 
     @classmethod
-    def add(cls, path, mimetype=None, unique=True):
+    def add(cls, name, mimetype=None, unique=True):
         """Add a new file uniquely"""
         if unique:
-            with open(path, 'rb') as file:
-                data = file.read()
-            sha256sum = str(sha256(data).hexdigest())
-            for record in cls.select().limit(1).where(cls.sha256sum
-                                                      == sha256sum):
+            with open(name, 'rb') as file:
+                cs = sha256sum(file.read())
+            for record in cls.select().limit(1).where(cls.sha256sum == cs):
                 record._hardlinks += 1
                 record.save()
                 return record
-        return cls._add(path, mimetype=mimetype)
+            else:
+                return cls._add(name, mimetype=mimetype)
+        else:
+            return cls._add(name, mimetype=mimetype)
 
     @classmethod
-    def _add(cls, path, mimetype=None):
+    def _add(cls, name, mimetype=None):
         """Forcibly adds a file"""
-        with open(path, 'rb') as file:
+        with open(name, 'rb') as file:
             data = file.read()
         sha256sum = str(sha256(data).hexdigest())
         record = cls()
-        record.basename = basename(path)
-        record.dirname = dirname(path)
+        record._name = name
         if mimetype is None:
-            record.mimetype = MIMEUtil.getmime(path)
+            record._mimetype = MIMEUtil.getmime(name)
         else:
-            record.mimetype = mimetype
+            record._mimetype = mimetype
         record._sha256sum = sha256sum
         record._size = len(data)
         record._hardlinks = 1
+        record.save()
         return record
+
+    @property
+    def name(self):
+        """Returns the file's name"""
+        return self._name
+
+    @property
+    def mimetype(self):
+        """Returns the file's MIME type"""
+        return self._mimetype
 
     @property
     def sha256sum(self):
@@ -102,20 +117,30 @@ class File(FileDBModel):
         return self._hardlinks
 
     @property
-    def name(self):
-        """The file's full name / path"""
-        return join(self.dirname, self.basename)
-
-    @property
     def path(self):
         """An alias to self.name"""
         return self.name
+
+    @property
+    def basename(self):
+        """Returns the file's basename"""
+        return basename(self.name)
+
+    @property
+    def dirname(self):
+        """Returns the file's dirname"""
+        return dirname(self.name)
 
     @property
     def data(self):
         """Returns the file's content"""
         with open(self.name, 'rb') as f:
             return f.read()
+
+    @property
+    def b64data(self):
+        """Returns the file's data base64 encoded"""
+        return b64encode(self.data)
 
     @property
     def consistent(self):
@@ -130,11 +155,11 @@ class File(FileDBModel):
     def read(self):
         """Reads the file's content safely"""
         data = self.data
-        sha256sum = str(sha256(data).hexdigest())
-        if sha256sum == self.sha256sum:
+        cs = sha256sum(data)
+        if cs == self.sha256sum:
             return data
         else:
-            raise ChecksumMismatch(sha256sum, self.sha256sum)
+            raise ChecksumMismatch(cs, self.sha256sum)
 
     def remove(self):
         """Removes the file"""
