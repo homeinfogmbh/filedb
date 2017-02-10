0f2c0bc8f25b09765a47a37cd419dae41811068e
Richard Neumann <r.neumann@homeinfo.de>
2017-02-10 18:16:15 +0100

    Simplified FileProperty

--- filedb/extra.py
+++ filedb/extra.py
@@ -7,33 +7,12 @@ __all__ = ['FileProperty']
 _param_err = ValueError('Need either file_client or key')
 
 
-class SaveCallback():
-    """Save callback wrapper"""
-
-    def __init__(self, instance, field):
-        self.instance = instance
-        self.field = field
-        self.old_save = self.instance.save
-
-    def __call__(self, *args, **kwargs):
-        if self.field.new_value is not None:
-            new_value = self.field.file_client.add(self.field.new_value)
-            self.field.new_value = None
-        else:
-            new_value = None
-
-        setattr(self.instance, self.field.integer_field.name, new_value)
-
-        if self.field.old_value is not None:
-            self.field.file_client.delete(self.field.old_value)
-            self.field.old_value = None
-
-        self.instance.__class__.save(self.instance)
-        self.instance.save = self.old_save
-
-
 class FileProperty():
-    """File property"""
+    """File property
+
+    XXX: Setting to file properties will save
+    the DB model for reasons of consistency.
+    """
 
     def __init__(self, integer_field, file_client=None, key=None):
         self.integer_field = integer_field
@@ -48,8 +27,6 @@ class FileProperty():
             raise _param_err
 
         self.file_client = file_client
-        self.old_value = None
-        self.new_value = None
 
     def __get__(self, instance, instance_type=None):
         """Returns file data from filedb using
@@ -68,13 +45,14 @@ class FileProperty():
         file_client and value from inter_field
         """
         if instance is not None:
-            if self.old_value is None:
-                self.old_value = getattr(instance, self.integer_field.name)
+            previous_value = getattr(instance, self.integer_field.name)
 
-            self.new_value = data
+            if previous_value is not None:
+                self.file_client.delete(previous_value)
 
-            if instance.save.__class__ is not SaveCallback:
-                instance.save = SaveCallback(instance, self)
+            if data is not None:
+                new_value = self.file_client.add(data)
+            else:
+                new_value = None
 
-    def delete(self, file_id):
-        """Deletes the old file"""
+            setattr(instance, self.integer_field.name, new_value)
