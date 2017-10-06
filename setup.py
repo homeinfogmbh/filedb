c62415ee5ebc162e0aab2c53cc1fd0118bf9ab40
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 12:11:57 +0200

    Setup refac.

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ from distutils.core import setup
 setup(
     name='filedb',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='info@homeinfo.de',
+    maintainer='Richard Neumann',
+    maintainer_email='r.neumann@homeinfo.de',
     packages=['filedb'],
+    scripts=['files/filedbutil'],
     data_files=[
-        ('/etc/uwsgi/apps-available',
-         ['files/etc/uwsgi/apps-available/filedb.ini']),
-        ('/usr/share/filedb',
-         ['files/usr/share/filedb/filedb.wsgi'])],
-    description='A file database')
+        ('/usr/local/bin', ['files/filedbutil']),
+        ('/etc/uwsgi/apps-available', ['files/filedb.ini']),
+        ('/usr/share/filedb', ['files/filedb.wsgi'])],
+    description='A file database.')
