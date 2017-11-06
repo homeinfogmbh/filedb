0c10a8b65bbf459cc5790927b7e7ce7b9e83e543
Richard Neumann <r.neumann@homeinfo.de>
2017-11-06 12:24:46 +0100

    Added base imports.

--- filedb/__init__.py
+++ filedb/__init__.py
@@ -1,6 +1,13 @@
 """HOMEINFO's file database"""
 
-from filedb.client import FileError, add, get, delete
+from filedb.client import FileError, add, get, delete, sha256sum, mimetype
 from filedb.extra import FileProperty
 
-__all__ = ['FileError', 'add', 'get', 'delete', 'FileProperty']
+__all__ = [
+    'FileError',
+    'add',
+    'get',
+    'delete',
+    'sha256sum',
+    'mimetype',
+    'FileProperty']
